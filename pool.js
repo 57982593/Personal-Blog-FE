5f469e26d2b6dd627c19c97ab76924f230c7ff60
v_zuwwang <57982593@qq.com>
2020-11-30 14:08:44 +0800

    init react demo

--- pool.js
+++
@@ -1,9 +0,0 @@
-const mysql=require('mysql');
-var pool=mysql.createPool({
-    host:'127.0.0.1',
-    port:3306,
-    user:'root',
-    database:'aww',
-    connectionLimit:20
-});
-module.exports=pool;
